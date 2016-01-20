736854945f8c388e7a461bf340dc8b1ff73f0be5
David Barri <japgolly@gmail.com>
2016-01-21 09:31:25 +1100

    eg test

--- core/shared/src/test/scala/teststate/WholeTest.scala
+++ core/shared/src/test/scala/teststate/WholeTest.scala
@@ -4,14 +4,34 @@ import utest._
 
 object WholeTest extends TestSuite {
 
-  case class Example(count: Int) {
-    def inc = copy(count + 1)
-    def dec = copy(count - 1)
+  class Example(start: Int) {
+    private var c = start
+    def inc() = c += 1
+    def dec() = c -= 1
+    def count() = c
   }
+
 
   override def tests = TestSuite {
 
-//    Runner.run()
+    val eg = new Example(3)
 
+    val inc =
+      Action.Single[Int, Int, Int, Int, String](_ => "Increment", (_, o) =>
+        Some(() => {
+          eg.inc()
+          Right((_: Int) + 1)
+        }),
+        Check[Int, Int, Int, Int, String, Int](_ => "Count increases by 1",
+          (_, o) => Right(o),
+          (_, o, n) => if (o == n + 1) None else Some(s"Expected ${n + 1}, not $o.")
+        )
+      )
+
+    println()
+    val h = Runner.run(inc)(3, () => eg.count())
+    println(h)
+    println(eg.count())
+    println()
   }
 }
