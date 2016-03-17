db16472d773fbd26ee433d4a97b8ba89b28b41f5
David Barri <japgolly@gmail.com>
2016-03-18 09:08:32 +1100

    Test >> associativity. Put in temp hack.

--- core/shared/src/main/scala/teststate/core/CoreComposition2.scala
+++ core/shared/src/main/scala/teststate/core/CoreComposition2.scala
@@ -4,9 +4,9 @@ import acyclic.file
 import teststate.data.Sack
 import teststate.typeclass.PolyComposable
 import Action.Actions
-import PolyComposable._
 import CoreExports._
 import CoreExports2._
+import PolyComposable.CanSeq
 
 /**
   * P = Point
@@ -42,17 +42,42 @@ object CoreComposition2 {
 
   trait Implicits {
 
-    implicit def actionsMonoComposable[F[_], R, O, S, E]: Mono[Actions[F, R, O, S, E]] =
+    implicit def actionsMonoComposable[F[_], R, O, S, E]: PolyComposable.Mono[Actions[F, R, O, S, E]] =
       PolyComposable(Sack.append)
 
-    implicit def actionsComposePreCond[F[_], R, O, S, E]: Right[Points[O, S, E], Actions[F, R, O, S, E]] =
-      PolyComposable((p, a) => a addCheck p.before)
+//    implicit def actionsComposePreCond[F[_], R, O, S, E]: PolyComposable.Right[Points[O, S, E], Actions[F, R, O, S, E]] =
+//      PolyComposable((p, a) => a addCheck p.before)
+//
+//    implicit def actionsComposePostCond[F[_], R, O, S, E]: PolyComposable.Left[Actions[F, R, O, S, E], Points[O, S, E]] =
+//      PolyComposable((a, p) => a addCheck p.after)
+//
+//    implicit def actionsComposePostCondA[F[_], R, O, S, E]: PolyComposable.Left[Actions[F, R, O, S, E], Arounds[O, S, E]] =
+//      PolyComposable((a, c) => a addCheck c)
 
-    implicit def actionsComposePostCond[F[_], R, O, S, E]: Left[Actions[F, R, O, S, E], Points[O, S, E]] =
-      PolyComposable((a, p) => a addCheck p.after)
+    import Sack._
 
-    implicit def actionsComposePostCondA[F[_], R, O, S, E]: Left[Actions[F, R, O, S, E], Arounds[O, S, E]] =
-      PolyComposable((a, c) => a addCheck c)
+    // Here be hacks... I want
+    // (A1 >> @1 >> A2 >> @2) to be interpretted as
+    // ((A1 >> @1) >> (A2 >> @2)) instead of
+    // ((A1 >> @1 >> A2) >> @2).
+
+    implicit def actionsComposePreCond[F[_], R, O, S, E]: PolyComposable.Right[Points[O, S, E], Actions[F, R, O, S, E]] =
+      PolyComposable((p, a) => a match {
+        case Product(ss) if ss.nonEmpty => Product((p >> ss.head) +: ss.tail)
+        case _                          => a addCheck p.before
+      })
+
+    implicit def actionsComposePostCond[F[_], R, O, S, E]: PolyComposable.Left[Actions[F, R, O, S, E], Points[O, S, E]] =
+      PolyComposable((a, p) => a match {
+        case Product(ss) if ss.nonEmpty => Product(ss.init :+ (ss.last >> p))
+        case _                          => a addCheck p.after
+      })
+
+    implicit def actionsComposePostCondA[F[_], R, O, S, E]: PolyComposable.Left[Actions[F, R, O, S, E], Arounds[O, S, E]] =
+      PolyComposable((a, c) => a match {
+        case Product(ss) if ss.nonEmpty => Product(ss.init :+ (ss.last >> c))
+        case _                          => a addCheck c
+      })
 
     implicit def actionsCanSeq[F[_], R, O, S, E]: CanSeq[Actions[F, R, O, S, E]] = CanSeq
 
--- core/shared/src/test/scala/teststate/CompositionTest.scala
+++ core/shared/src/test/scala/teststate/CompositionTest.scala
@@ -1,9 +1,33 @@
 package teststate
 
+import nyaya.prop._
+import nyaya.test.PropTest._
+import teststate.typeclass.PolyComposable
+import teststate.typeclass.PolyComposable.CanSeq
+import scalaz.std.string.stringInstance
 import utest._
-import teststate.Exports._
+import Exports._
+import RandomData.*
 
 object CompositionTest extends TestSuite {
+
+
+  def associativity[A, B, C, AB, BC](implicit
+                                     x1: PolyComposable[A, B, AB],
+                                     x2: PolyComposable[B, C, BC],
+                                     x3: PolyComposable[AB, C, *.Action],
+                                     x4: PolyComposable[A, BC, *.Action]) = {
+
+    implicit def anyCanSeq[X]: CanSeq[X] = CanSeq
+
+    def results(a: *.Action): String =
+      "\n" + Test(a).observe(_ => ()).run((), ()).format(History.Options.uncolored)
+
+    Prop.equal[(A, B, C)]("associativity")(
+      { case (a, b, c) => results((a >> b) >> c) },
+      { case (a, b, c) => results(a >> (b >> c)) })
+  }
+
   override def tests = TestSuite {
     'coproduct {
       import CoproductExample._
@@ -11,7 +35,25 @@ object CompositionTest extends TestSuite {
       val top = new Top(7, "e")
       val h = test.run(State(Type.Num, 7, "e"), top)
       h.assert(History.Options.colored)
-      println(h.format(History.Options.colored.alwaysShowChildren))
+      // println(h.format(History.Options.colored.alwaysShowChildren))
     }
+
+    import RandomData._
+    "a >> a >> a" -
+      action.triple.mustSatisfy(associativity) //(defaultPropSettings.setDebug)
+
+    /*
+    associativity doesn't hold for the following:
+    Come to think of it, I wouldn't want it to. If I deliberately group somethings together I wouldn't want it unpacked.
+
+    "a >> p >> a" -
+      Gen.tuple3(action, point, action).mustSatisfy(associativity)(defaultPropSettings.setDebug.setSeed(1).setSingleThreaded)
+
+    "a >> a >> p" -
+      Gen.tuple3(action, action, point).mustSatisfy(associativity)(defaultPropSettings.setDebug.setSeed(1).setSingleThreaded)
+
+    "p >> a >> a" -
+      Gen.tuple3(point, action, action).mustSatisfy(associativity)(defaultPropSettings.setDebug.setSeed(1).setSingleThreaded)
+    */
   }
 }
