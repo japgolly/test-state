74822b89a731c3763d94baded2b370d761748a71
David Barri <japgolly@gmail.com>
2016-04-02 13:46:31 +1100

    Export Sizzle itself

--- dom-zipper-sizzle/src/main/scala/teststate/domzipper/sizzle/Exports.scala
+++ dom-zipper-sizzle/src/main/scala/teststate/domzipper/sizzle/Exports.scala
@@ -2,7 +2,11 @@ package teststate.domzipper.sizzle
 
 import teststate.domzipper.DomZipper.CssSelEngine
 
-trait Exports extends teststate.domzipper.Exports {
+trait Exports
+  extends teststate.domzipper.Exports {
+
+  type Sizzle = teststate.domzipper.sizzle.Sizzle.type
+  val Sizzle = teststate.domzipper.sizzle.Sizzle
 
   implicit val cssSelEngine: CssSelEngine =
     CssSelEngine(Sizzle(_, _))
