7dbbb937d15976fb2d2c4f6fc726078ddaca6aec
David Barri <japgolly@gmail.com>
2016-05-05 11:37:12 +1000

    Refactor: Rename {TestState ⇒ Ext}Nyaya

--- ext-nyaya/shared/src/main/scala/teststate/ExtNyaya.scala
+++ ext-nyaya/shared/src/main/scala/teststate/ExtNyaya.scala
@@ -5,18 +5,18 @@ import nyaya.gen._
 import nyaya.test.Settings
 import teststate.data.ROS
 import Exports._
-import TestStateNyaya._
+import ExtNyaya._
 
-trait TestStateNyaya {
+trait ExtNyaya {
   implicit def toDslNyayaOps[F[_], R, O, S, E](dsl: Dsl[F, R, O, S, E]): DslNyayaOps[F, R, O, S, E] =
     new DslNyayaOps(dsl)
 }
 
-object TestStateNyaya extends TestStateNyaya {
+object ExtNyaya extends ExtNyaya {
 
   final class DslNyayaOps[F[_], R, O, S, E](private val dsl: Dsl[F, R, O, S, E]) extends AnyVal with Dsl.Types[F, R, O, S, E] {
     def genActions(name: ActionName)(g: Gen[Actions])(implicit s: Settings): Actions =
-      TestStateNyaya.genActions(g)(name)(s)
+      ExtNyaya.genActions(g)(name)(s)
 
     def genActionsBy(name: ActionName)(g: ROS => Gen[Actions])(implicit s: Settings): Actions =
       dsl.chooseAction(name)(i => genActions(name)(g(i))(s))
