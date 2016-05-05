fa99f5748848af3bdc86b5726f3f7206cd4aa3dd
David Barri <japgolly@gmail.com>
2016-05-05 11:42:53 +1000

    Refactor: Reorder Exports contents

--- core/shared/src/main/scala/teststate/Exports.scala
+++ core/shared/src/main/scala/teststate/Exports.scala
@@ -11,14 +11,14 @@ trait Exports
      with typeclass.Equal.Implicits
      with run.RunImplicits {
 
+  type Display[E] = teststate.typeclass.Display[E]
+  val Display = teststate.typeclass.Display
+
+  type DisplayError[E] = teststate.typeclass.DisplayError[E]
+  val DisplayError = teststate.typeclass.DisplayError
+
   type Dsl[F[_], R, O, S, E] = teststate.dsl.Dsl[F, R, O, S, E]
   val Dsl = teststate.dsl.Dsl
-
-  type Recover[+E] = teststate.typeclass.Recover[E]
-  val Recover = teststate.typeclass.Recover
-
-  type ExecutionModel[M[_]] = teststate.typeclass.ExecutionModel[M]
-  val ExecutionModel = teststate.typeclass.ExecutionModel
 
   type Empty[+E] = teststate.typeclass.Empty[E]
   val Empty = teststate.typeclass.Empty
@@ -26,11 +26,8 @@ trait Exports
   type Equal[E] = teststate.typeclass.Equal[E]
   val Equal = teststate.typeclass.Equal
 
-  type Display[E] = teststate.typeclass.Display[E]
-  val Display = teststate.typeclass.Display
-
-  type DisplayError[E] = teststate.typeclass.DisplayError[E]
-  val DisplayError = teststate.typeclass.DisplayError
+  type ExecutionModel[M[_]] = teststate.typeclass.ExecutionModel[M]
+  val ExecutionModel = teststate.typeclass.ExecutionModel
 
   type Name = teststate.data.Name
   val Name = teststate.data.Name
@@ -38,26 +35,29 @@ trait Exports
   type NameFn[-A] = teststate.data.NameFn[A]
   val NameFn = teststate.data.NameFn
 
+  type Observer[-R, +O, +E] = teststate.run.Observer[R, O, E]
+  val Observer = teststate.run.Observer
+
   type Plan[F[_], R, O, S, E] = teststate.run.Plan[F, R, O, S, E]
   val Plan = teststate.run.Plan
 
   type PlanWithInitialState[F[_], R, O, S, E] = teststate.run.PlanWithInitialState[F, R, O, S, E]
   val PlanWithInitialState = teststate.run.PlanWithInitialState
 
+  type Recover[+E] = teststate.typeclass.Recover[E]
+  val Recover = teststate.typeclass.Recover
+
+  type Report[+E] = teststate.run.Report[E]
+  val Report = teststate.run.Report
+
+  type Result[+E] = teststate.data.Result[E]
+  val Result = teststate.data.Result
+
   type Test[F[_], R, O, S, E] = teststate.run.Test[F, R, O, S, E]
   val Test = teststate.run.Test
 
   type TestWithInitialState[F[_], R, O, S, E] = teststate.run.TestWithInitialState[F, R, O, S, E]
   val TestWithInitialState = teststate.run.TestWithInitialState
-
-  type Observer[-R, +O, +E] = teststate.run.Observer[R, O, E]
-  val Observer = teststate.run.Observer
-
-  type Result[+E] = teststate.data.Result[E]
-  val Result = teststate.data.Result
-
-  type Report[+E] = teststate.run.Report[E]
-  val Report = teststate.run.Report
 
   implicit def testStateOrFromScalaEither[A, B](e: A Either B): A Or B =
     Or fromScalaEither e
