03880f39c1efd0c294393fd6e9af8d0138fd489c
David Barri <japgolly@gmail.com>
2016-09-03 16:18:14 +1000

    Towards 2.1.1

--- project/TestState.scala
+++ project/TestState.scala
@@ -45,7 +45,7 @@ object TestState {
   val commonSettings = ConfigureBoth(
     _.settings(
       organization             := "com.github.japgolly.test-state",
-      version                  := "2.1.0",
+      version                  := "2.1.1-SNAPSHOT",
       homepage                 := Some(url("https://github.com/japgolly/" + ghProject)),
       licenses                 += ("Apache-2.0", url("http://opensource.org/licenses/Apache-2.0")),
       scalaVersion             := Ver.Scala211,
