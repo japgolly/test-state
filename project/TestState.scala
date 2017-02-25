ad20147c30a90a49938ed46ab3cb907a1c033b42
David Barri <japgolly@gmail.com>
2017-02-25 12:32:26 +1100

    Upgrade dependencies

--- project/TestState.scala
+++ project/TestState.scala
@@ -16,18 +16,18 @@ object TestState {
     final val Cats          = "0.8.1"
     final val KindProjector = "0.9.3"
     final val MacroParadise = "2.1.0"
-    final val MTest         = "0.4.4"
+    final val MTest         = "0.4.5"
     final val Nyaya         = "0.8.1"
     final val Scala211      = "2.11.8"
-    final val Scala212      = "2.12.0"
+    final val Scala212      = "2.12.1"
     final val ScalaJsDom    = "0.9.1"
     final val ScalaJsReact  = "0.11.3"
-    final val Scalaz        = "7.2.7"
+    final val Scalaz        = "7.2.9"
     final val Sizzle        = "2.3.0"
     final val UnivEq        = "1.0.2"
 
     // Used in examples only
-    final val Monocle       = "1.3.2"
+    final val Monocle       = "1.4.0"
     final val ReactJs       = "15.3.2"
   }
 
@@ -48,7 +48,7 @@ object TestState {
       organization             := "com.github.japgolly.test-state",
       homepage                 := Some(url("https://github.com/japgolly/" + ghProject)),
       licenses                 += ("Apache-2.0", url("http://opensource.org/licenses/Apache-2.0")),
-      scalaVersion             := Ver.Scala211,
+      scalaVersion             := Ver.Scala212,
       crossScalaVersions       := Seq(Ver.Scala211, Ver.Scala212),
       scalacOptions           ++= scalacFlags,
       scalacOptions in Test   --= Seq("-Ywarn-dead-code"),
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,5 +1,5 @@
-addSbtPlugin("org.scala-js"     % "sbt-scalajs"          % "0.6.13")
-addSbtPlugin("com.typesafe.sbt" % "sbt-pgp"              % "0.8.3")
+addSbtPlugin("org.scala-js"     % "sbt-scalajs"          % "0.6.14")
+addSbtPlugin("com.jsuereth"     % "sbt-pgp"              % "1.0.1")
 addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.8.2")
-addSbtPlugin("com.timushev.sbt" % "sbt-updates"          % "0.2.0")
+addSbtPlugin("com.timushev.sbt" % "sbt-updates"          % "0.3.0")
 
