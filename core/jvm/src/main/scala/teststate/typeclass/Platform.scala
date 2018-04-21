1abf010ea4884350653d1135a6f2b146d9b67747
David Barri <japgolly@gmail.com>
2018-04-21 11:08:08 +1000

    Surround Thread.sleep in blocking()

--- core/jvm/src/main/scala/teststate/typeclass/Platform.scala
+++ core/jvm/src/main/scala/teststate/typeclass/Platform.scala
@@ -1,8 +1,10 @@
 package teststate.typeclass
 
+import scala.concurrent.blocking
+
 private[typeclass] object Platform {
 
   def threadSleep(timeMs: Long): Unit =
-    Thread.sleep(timeMs)
+    blocking(Thread.sleep(timeMs))
 
 }
