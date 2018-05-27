682db83ee1bd75b79a137d0c347c88ce20a70df8
David Barri <japgolly@gmail.com>
2018-05-27 10:51:49 +1000

    Use microlibs test-util in core tests

--- core/shared/src/test/scala/teststate/TestUtils.scala
+++ core/shared/src/test/scala/teststate/TestUtils.scala
@@ -1,18 +1,14 @@
 package teststate
 
-import scala.io.AnsiColor._
 import Exports._
 
 object TestUtil extends TestUtil
 
-case class ActualExpect[A](actual: A, expect: A) {
-  def map[B](f: A => B) = ActualExpect[B](f(actual), f(expect))
+trait TestUtil
+  extends japgolly.microlibs.testutil.TestUtil {
 
-  def forall(f: A => Boolean) = f(actual) && f(expect)
-  def exists(f: A => Boolean) = f(actual) || f(expect)
-}
-
-trait TestUtil {
+  implicit def testStateEqualityToScalaz[A](implicit e: Equal[A]): scalaz.Equal[A] =
+    scalaz.Equal.equal(e.equal)
 
   val inspectionBaseSettings =
     Report.Format.Default
@@ -28,61 +24,6 @@ trait TestUtil {
   val inspectionAS =
     Report.AssertionSettings.uniform(inspectionFormat)
 
-  def assertEq[A: Equal](actual: A, expect: A): Unit =
-    assertEq(null, actual, expect)
-
-  def assertEq[A](name: => String, actual: A, expect: A)(implicit eq: Equal[A]): Unit =
-    if (!eq.equal(actual, expect)) {
-      println()
-      Option(name).foreach(n => println(s">>>>>>> $n"))
-
-      val toString: Any => String = {
-        case s: Stream[_] => s.force.toString() // SI-9266
-        case a            => a.toString
-      }
-
-      val strs = ActualExpect(actual, expect) map toString
-      val lines = strs.map(_ split '\n')
-
-      if (lines.actual.length == lines.expect.length && lines.actual.length > 1) {
-        // Print side-by-side
-        val norm = lines.map(_.map(_.replace("\t", "\\t")))
-        val maxs = norm.map(_.iterator.map(_.length).max max 6)
-        val fmt = s"%s%-${maxs.expect}s$RESET | %s%s$RESET"
-        val sep = s"${"-" * maxs.expect}-+-${"-" * maxs.actual}"
-        val B = BOLD + RED
-        val G = ""
-        println(sep)
-        println(fmt.format(G, "Expect", G, "Actual"))
-        println(sep)
-        for ((l, r) <- norm.expect zip norm.actual) {
-          println(if (l == r)
-              fmt.format(G, l, G, r)
-          else
-              fmt.format(B, l, B, r))
-        }
-        println(sep)
-
-      } else {
-        var pre = "["
-        val post = "]"
-        if ((strs.actual + strs.expect) contains "\n") {
-          pre = "↙[\n"
-        }
-        println(s"expect: $pre$BOLD$BLUE${strs.expect}$RESET$post")
-        println(s"actual: $pre$BOLD$RED${strs.actual}$RESET$post")
-      }
-
-      println()
-      fail("assertEq failed.")
-    }
-
-  def fail(msg: String): Nothing = {
-    val e = new AssertionError(msg)
-    e.setStackTrace(Array.empty)
-    throw e
-  }
-
   def assertDefined[A](o: Option[A], expectDefined: Boolean): Unit =
     assertDefined(null, o, expectDefined)
 
@@ -90,7 +31,7 @@ trait TestUtil {
     if (expectDefined)
       assertEq(name, o.isDefined, true)
     else
-      assertEq(name, o, None)(Equal.by_==)
+      assertEq(name, o, None)(scalaz.Equal.equalA)
 
   val trim = (_: String).trim
   val stringIdFn = (s: String) => s
@@ -103,6 +44,13 @@ trait TestUtil {
     val n = normalise compose trim
     val f = if (showChildren) inspectionFormat else inspectionFormatOnlyFailedChildren
     val actual = r.format(f)(s)
-    assertEq(actual = n(actual), n(expect))
+    assertMultiline(actual = n(actual), n(expect))
   }
+}
+
+case class ActualExpect[A](actual: A, expect: A) {
+  def map[B](f: A => B) = ActualExpect[B](f(actual), f(expect))
+
+  def forall(f: A => Boolean) = f(actual) && f(expect)
+  def exists(f: A => Boolean) = f(actual) || f(expect)
 }
