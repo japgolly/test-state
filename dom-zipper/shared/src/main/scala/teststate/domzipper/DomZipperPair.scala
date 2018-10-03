fd1f9b39490ebdfcd44e18d309bb38b6d9654aae
David Barri <japgolly@gmail.com>
2018-10-04 06:50:09 +1000

    move Store

--- dom-zipper/shared/src/main/scala/teststate/domzipper/DomZipperPair.scala
+++ dom-zipper/shared/src/main/scala/teststate/domzipper/DomZipperPair.scala
@@ -2,31 +2,6 @@ package teststate.domzipper
 
 import ErrorHandler._
 
-final class Store[S, A](val pos: S, val peek: S => A) {
-  override def toString: String =
-    s"Store($pos)($peek)"
-
-  def map[B](f: A => B): Store[S, B] =
-    Store(pos)(f compose peek)
-
-  def extract: A =
-    peek(pos)
-
-  def duplicate: Store[S, Store[S, A]] =
-    Store(pos)(Store(_)(peek))
-
-  def extend[B](f: Store[S, A] => B): Store[S, B] =
-    duplicate.map(f)
-}
-
-object Store {
-  def apply[S, A](pos: S)(peek: S => A): Store[S, A] =
-    new Store(pos, peek)
-}
-
-
-// =====================================================================================================================
-
 object DomZipperPair {
   def apply[
     F[_],
