853b02d89024079d786ab8f951019c4a3b7f0aac
David Barri <japgolly@gmail.com>
2018-10-04 06:50:28 +1000

    Revert "Eval"
    This reverts commit 088c67042a6e71ba0639968834069866ccf36e8f.

--- dom-zipper/shared/src/main/scala/teststate/domzipper/DomZipperPair.scala
+++ dom-zipper/shared/src/main/scala/teststate/domzipper/DomZipperPair.scala
@@ -10,16 +10,11 @@ object DomZipperPair {
     Slow[f[_]] <: DomZipper[f, A, Slow]
   ](
      fast: Fast[F],
-     slow: Slow[F],
-     evalStrategy: Eval.Strategy
+     slow: Slow[F]
    )(
     implicit F: ErrorHandler[F]
    ): DomZipperPair[F, () => F[A]] =
-    full[F, () => F[A], Fast, Slow](
-      fast,
-      Store(slow)(s => Eval.value(F pass s)),
-      g => () => g().map(_.dom),
-      evalStrategy)
+    full[F, () => F[A], Fast, Slow](fast, Store(slow)(F.pass), g => () => g().map(_.dom))
 
   def full[
     F[_],
@@ -28,9 +23,8 @@ object DomZipperPair {
     _SlowF[f[_]] <: DomZipper[f, _, _SlowF]
   ](
     _fast: _FastF[F],
-    _slow: Store[_SlowF[F], Eval[F[_SlowF[F]]]],
-    _domFn: (() => F[_SlowF[F]]) => A,
-    _evalStrategy: Eval.Strategy
+    _slow: Store[_SlowF[F], F[_SlowF[F]]],
+    _domFn: (() => F[_SlowF[F]]) => A
    )(
     implicit _F: ErrorHandler[F]
    ): DomZipperPair[F, A] =
@@ -40,7 +34,6 @@ object DomZipperPair {
       override protected val fast = _fast
       override protected val slow = _slow
       override protected val domFn = _domFn
-      override protected val evalStrategy = _evalStrategy
       override protected implicit val F = _F
     }
 }
@@ -57,16 +50,15 @@ trait DomZipperPair[F[_], A] extends DomZipper2[F, A, λ[G[_] => DomZipperPair[G
   protected final type Slow = SlowF[F]
 
   protected val fast: Fast
-  protected val slow: Store[Slow, Eval[F[Slow]]]
+  protected val slow: Store[Slow, F[Slow]]
   protected val domFn: (() => F[Slow]) => A
-  protected val evalStrategy: Eval.Strategy
   protected implicit val F: ErrorHandler[F]
 
   def getAttribute(name: String): Option[String] =
     fast.getAttribute(name)
 
   def dom: A =
-    domFn(() => slow.extract.eval())
+    domFn(() => slow.extract)
 
   def apply(css: String): F[DomZipperPair[F, A]] =
     zmap(_(css), _(css))
@@ -74,9 +66,6 @@ trait DomZipperPair[F[_], A] extends DomZipper2[F, A, λ[G[_] => DomZipperPair[G
   def parent: F[DomZipperPair[F, A]] =
     zmap(_.parent, _.parent)
 
-  private def flatMapSlow(f: Slow => F[Slow]): Store[Slow, Eval[F[Slow]]] =
-    slow.map(efa => evalStrategy(efa.eval().flatMap(f)))
-
   private def zmap(f: Fast => F[Fast], s: Slow => F[Slow]): F[DomZipperPair[F, A]] =
-    f(fast).map(DomZipperPair.full[F, A, FastF, SlowF](_, flatMapSlow(s), domFn, evalStrategy))
+    f(fast).map(DomZipperPair.full[F, A, FastF, SlowF](_, slow.map(_.flatMap(s)), domFn))
 }
