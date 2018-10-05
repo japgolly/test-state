89c7d9d32be3ac55e10016b6a2f25f59d222db66
David Barri <japgolly@gmail.com>
2018-10-05 18:35:34 +1000

    DomZipperPair.children1n

--- dom-zipper/shared/src/main/scala/teststate/domzipper/DomZipperPair.scala
+++ dom-zipper/shared/src/main/scala/teststate/domzipper/DomZipperPair.scala
@@ -61,17 +61,6 @@ trait DomZipperPair[F[_], A] extends DomZipper[F, A, λ[G[_] => DomZipperPair[G,
   private def zmap(f: Fast => F[Fast], s: Slow => F[Slow]): F[DomZipperPair[F, A]] =
     f(fast).map(DomZipperPair.full[F, A, FastF, SlowF, S](_, flatMapSlow(s), domFn))
 
-  /*
-  def getAttribute(name: String): Option[String] =
-    fast.getAttribute(name)
-
-  def apply(css: String): F[DomZipperPair[F, A]] =
-    zmap(_(css), _(css))
-
-  def parent: F[DomZipperPair[F, A]] =
-    zmap(_.parent, _.parent)
-  */
-
   override def describe = fast.describe
 
   // ==================
@@ -117,69 +106,19 @@ trait DomZipperPair[F[_], A] extends DomZipper[F, A, λ[G[_] => DomZipperPair[G,
   override def children0n(sel: String) = ??? //: DomCollection[Self, F, Vector, A]
   //override def children1n(sel: String) = //: DomCollection[Self, F, Vector, A]
 
-  // Dom type here should be fast dom?
-  //      - [N] .filter uses zippers
-  //      - [N] .traverse should be .traverseDoms
-  //      - [N] .doms should return slow doms, dom access is the exit point for this zipper
-  // TODO Collection zipper type mismatch
-  //      - Either: Map from FastZipper => PairZipper
-  //      -     or: add newDomCollection that starts with PairZipper and delegates to Fast internally
-  // TODO Collection dom type mismatch
-
-
-  // idea: Change rawResults so that it includes the original child index
-  // such that it is preserved after filter) and then pass that through the addLayer function
-  // to just select child(sel, n of size)
-
-  // efficiency: retain dom type S of slow path, extract slow.children01
-  // = F[Vector[ S x SlowZipper[S] ]]
-  // = turn F[S | SlowZipper[S]] => A
-
   type FfsIntellij[G[_]] = DomZipperPair[G, A]
-////  override def children1n(sel: String): DomCollection[λ[G[_] => DomZipperPair[G, A]], F, Vector, A] =
+//  override def children1n(sel: String): DomCollection[λ[G[_] => DomZipperPair[G, A]], F, Vector, A] =
   override def children1n(sel: String): DomCollection[FfsIntellij, F, Vector, A] = {
     val fastC = fast.children1n(sel)
     val size = fastC.size
-    val slowCS = slow.map(_.map(_.children1n(sel)))
-    lazy val slowC = slowCS.extract
-    val rawResults = Vector.tabulate(size)(i => i -> domFn(() => slowC.map(_.rawResult(i)._2)))
-
-    val addLayerFn: (MofN, DomZipper.Layer[A]) => FfsIntellij[F] =
-      (mOfN, l) => {
-        val i = mOfN.m
-        val fd = fastC.rawResult(i)._2
-        val f = fastC.addLayerFn(mOfN, l.copy(dom = fd))
-        val s = Store.fromUnit(slowC.flatMap(_.zippers).map(_(i)))
-        DomZipperPair.full[F, A, FastF, SlowF, S](f, s, domFn)
-      }
-
+    lazy val slowC = slow.map(_.map(_.children1n(sel))).extract
+    val rawResults = Vector.tabulate(size) { i =>
+      val f = fastC.rawResults(i)
+      val s = Store.fromUnit(slowC.flatMap(_.zippers).map(_(i)))
+      DomZipperPair.full[F, A, FastF, SlowF, S](f, s, domFn)
+    }
 
     new DomCollection[FfsIntellij, F, Vector, A](
-      addLayerFn = addLayerFn,
-      desc       = fastC.desc,
-      rawResult  = rawResults,
-      filterFn   = None,
-      C          = new DomCollection.Container1N[F]
-    )
-  }
-
-
-  import DomZipper.DomCollection2
-  def children1n_v2(sel: String): DomCollection2[FfsIntellij, F, Vector, A] = {
-    val fastC = fast.children1n_v2(sel)
-    val size = fastC.size
-    val slowCS = slow.map(_.map(_.children1n_v2(sel)))
-    lazy val slowC = slowCS.extract
-    val rawResults = Vector.tabulate(size)(i =>
-      //i -> domFn(() => slowC.map(_.rawResults(i).dom))
-      {
-        val f = fastC.rawResults(i)
-        val s = Store.fromUnit(slowC.flatMap(_.zippers).map(_(i)))
-        DomZipperPair.full[F, A, FastF, SlowF, S](f, s, domFn)
-      }
-    )
-
-    new DomCollection2[FfsIntellij, F, Vector, A](
       desc       = fastC.desc,
       rawResults = rawResults,
       filterFn   = None,
