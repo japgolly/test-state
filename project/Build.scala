7938d1770ee5d4d11f2f9de7ae7a975fbc00f4ce
Scala Steward <me@scala-steward.org>
2020-04-29 16:35:57 +0200

    Update scalaz-core to 7.3.0

--- project/Build.scala
+++ project/Build.scala
@@ -32,7 +32,7 @@ object TestState {
     val ScalaJsDom      = "0.9.8"
     val ScalaJsReact    = "1.6.0"
     val ScalaJsJavaTime = "0.2.6"
-    val Scalaz          = "7.2.30"
+    val Scalaz          = "7.3.0"
     val Selenium        = "3.141.59"
     val Sizzle          = "2.3.0"
     val UnivEq          = "1.2.0"
