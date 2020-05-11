54b62d364f90519ba9f59a5ca483ee3715faec4c
David Barri <japgolly@gmail.com>
2020-05-11 16:02:01 +1000

    Update deps

--- project/Build.scala
+++ project/Build.scala
@@ -23,23 +23,23 @@ object TestState {
     val Jsoup           = "1.13.1"
     val KindProjector   = "0.11.0"
     val MacroParadise   = "2.1.1"
-    val Microlibs       = "2.0"
-    val MTest           = "0.7.1"
-    val Nyaya           = "0.9.0"
+    val Microlibs       = "2.3"
+    val MTest           = "0.7.4"
+    val Nyaya           = "0.9.2"
     val Scala212        = "2.12.11"
-    val Scala213        = "2.13.1"
-    val ScalaCollCompat = "2.1.4"
-    val ScalaJsDom      = "0.9.8"
-    val ScalaJsReact    = "1.6.0"
-    val ScalaJsJavaTime = "0.2.6"
+    val Scala213        = "2.13.2"
+    val ScalaCollCompat = "2.1.6"
+    val ScalaJsDom      = "1.0.0"
+    val ScalaJsReact    = "1.7.0"
+    val ScalaJsJavaTime = "1.0.0"
     val Scalaz          = "7.2.30"
     val Selenium        = "3.141.59"
     val Sizzle          = "2.3.0"
-    val UnivEq          = "1.2.0"
+    val UnivEq          = "1.2.1"
 
     // Used in examples only
     val Monocle         = "2.0.4"
-    val ReactJs         = "16.7.0"
+    val ReactJs         = "16.13.1"
   }
 
   def scalacFlags = Seq(
