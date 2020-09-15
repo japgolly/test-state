37fe641dacb0b5d523ccc46855959549d9604e0d
David Barri <japgolly@gmail.com>
2020-09-15 14:30:34 +1000

    Upgrade most deps

--- project/Lib.scala
+++ project/Lib.scala
@@ -7,6 +7,7 @@ import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
 import sbtcrossproject.CrossPlugin.autoImport._
 import sbtcrossproject.CrossProject
 import scalajscrossproject.ScalaJSCrossPlugin.autoImport._
+import xerial.sbt.Sonatype.autoImport._
 
 object Lib {
   type CPE = CrossProject => CrossProject
@@ -38,13 +39,7 @@ object Lib {
   def publicationSettings(ghProject: String) =
     ConfigureBoth(
       _.settings(
-        publishTo := {
-          val nexus = "https://oss.sonatype.org/"
-          if (isSnapshot.value)
-            Some("snapshots" at nexus + "content/repositories/snapshots")
-          else
-            Some("releases"  at nexus + "service/local/staging/deploy/maven2")
-        },
+        publishTo := sonatypePublishToBundle.value,
         pomExtra :=
           <scm>
             <connection>scm:git:github.com/japgolly/{ghProject}</connection>
