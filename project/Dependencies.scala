be3e156fae371ddf2f72b3f423d219edc315d1bd
Scala Steward <me@scala-steward.org>
2021-08-15 11:01:55 +0200

    Update jsoup to 1.14.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
 
     // Exported
     val cats            = "2.6.1"
-    val jsoup           = "1.14.1"
+    val jsoup           = "1.14.2"
     val microlibs       = "3.0"
     val nyaya           = "0.11.0"
     val scala2          = "2.13.6"
