69699a575a43357ee0bb30e75c39c5e017a3a8be
David Barri <japgolly@gmail.com>
2021-10-27 11:25:08 +1100

    Upgrade scalajs-dom to v2, and all supporting japgolly libs (#239)

--- dom-zipper/js/src/main/scala/teststate/domzipper/JsDomExt.scala
+++ dom-zipper/js/src/main/scala/teststate/domzipper/JsDomExt.scala
@@ -2,24 +2,15 @@ package teststate.domzipper
 
 import org.scalajs.dom
 import org.scalajs.dom.html
-import scala.scalajs.js
 import teststate.util.CssUtil
 
 trait JsDomExt {
 
   implicit def testStateExtJsDomDocument(a: dom.Document): JsDomExt.DocumentExt = new JsDomExt.DocumentExt(a)
-  implicit def testStateExtJsDomElement (a: dom.Element) : JsDomExt.ElementExt  = new JsDomExt.ElementExt(a)
-  implicit def testStateExtJsDomNodeList(a: dom.NodeList): JsDomExt.NodeListExt = new JsDomExt.NodeListExt(a)
 
 }
 
 object JsDomExt extends JsDomExt {
-
-  private val matchesFn: (dom.Element, String) => Boolean =
-    if (js.Dynamic.global.Element.prototype.matches.asInstanceOf[js.UndefOr[Any]].isDefined)
-      (e, s) => e.asInstanceOf[js.Dynamic].matches(s).asInstanceOf[Boolean]
-    else
-      (e, s) => e.asInstanceOf[js.Dynamic].msMatchesSelector(s).asInstanceOf[Boolean]
 
   final class DocumentExt(private val self: dom.Document) extends AnyVal {
     def addStyleTag(content: String): Unit = {
@@ -39,16 +30,6 @@ object JsDomExt extends JsDomExt {
         disableTransforms  = disableTransforms)
       addStyleTag(css)
     }
-  }
-
-  final class ElementExt(private val self: dom.Element) extends AnyVal {
-    def matches(sel: String): Boolean =
-      matchesFn(self, sel)
-  }
-
-  final class NodeListExt(private val self: dom.NodeList) extends AnyVal {
-    def iterator: Iterator[dom.Node] =
-      (0 until self.length).iterator.map(self.apply)
   }
 
 }
