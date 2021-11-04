880a9b490c1f53bb0a04bd738fd70a39f53f7590
David Barri <japgolly@gmail.com>
2021-11-05 09:40:16 +1100

    Upgrade scalajs-react to 2.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
     val scala2          = "2.13.6"
     val scala3          = "3.0.2"
     val scalaJsDom      = "2.0.0"
-    val scalaJsReact    = "2.0.0-RC4"
+    val scalaJsReact    = "2.0.0"
     val selenium        = "3.141.59"
     val sizzle          = "2.3.0"
     val univEq          = "2.0.0"
