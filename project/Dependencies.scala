c1f35ee0c34482f2550f8219b9cf898993ff6187
Scala Steward <me@scala-steward.org>
2022-05-13 09:26:42 +0200

    Update scalajs-dom to 2.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     val nyaya           = "1.0.0"
     val scala2          = "2.13.6"
     val scala3          = "3.0.2"
-    val scalaJsDom      = "2.0.0"
+    val scalaJsDom      = "2.2.0"
     val scalaJsReact    = "2.0.0"
     val selenium        = "3.141.59"
     val sizzle          = "2.3.0"
